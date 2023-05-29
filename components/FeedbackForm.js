0177471e686442e6160ed6a396e94663ce301a30
zdamen <zdamen@umich.edu>
2023-05-29 16:17:27 -0400

    added reading from mongodb

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const FeedbackForm = () => {
     const [feedback, setFeedback] = useState('');
+    const [displayFeedback, setDisplayFeedback] = useState(null);
 
     const handleChange = (event) => {
         setFeedback(event.target.value);
@@ -21,13 +22,26 @@ const FeedbackForm = () => {
         if (response.ok) {
             alert('Feedback submitted!');
             setFeedback('');
+            
         } else {
             alert('An error occurred while submitting feedback.');
         }
     };
 
+    const fetchFeedback = async () => {
+        const res = await fetch('/api/getFeedback');
+        const data = await res.json();
+        setDisplayFeedback(data.feedback);
+    };
+    
+    const handleClick = async () => {
+        const res = await fetch('/api/getFeedback');
+        const data = await res.json();
+        setDisplayFeedback(data.feedback);
+    };
+
     return (
-        <div  style={{width: '80%', marginLeft: '9%', marginRight: '12%'} }>
+        <div style={{width: '80%', marginLeft: '9%', marginRight: '12%'} }>
             <form onSubmit={handleSubmit}>
                 <h2>Leave feedback</h2>
                 <textarea 
@@ -37,7 +51,7 @@ const FeedbackForm = () => {
                     required
                     style={{width: '100%', minHeight: '200px'}}
                 />
-                 <div style={{textAlign: 'right'}}>
+                <div  style={{textAlign: 'right'}}>
                     <button 
                         type="submit" 
                         style={{
@@ -49,12 +63,34 @@ const FeedbackForm = () => {
                             fontSize: '1em',
                             cursor: 'pointer',
                             marginTop: '10px',
+                            marginRight: '10px',
                         }}
                     >
                         Submit
                     </button>
                 </div>
             </form>
+            <div className='text-right'>
+            <button 
+                        onClick={handleClick}
+                        style={{
+                            backgroundColor: '#007BFF', 
+                            color: '#fff',
+                            padding: '10px 20px',
+                            border: 'none',
+                            borderRadius: '5px',
+                            fontSize: '1em',
+                            cursor: 'pointer',
+                            marginTop: '10px',
+                        }}
+                    >
+                        Get Latest Feedback
+                    </button>
+                    <div className='text-center'>
+                    {displayFeedback && <p className='text-3xl'> Latest Feedback: {displayFeedback}</p>}
+                    </div>
+            
+            </div>
         </div>
     );
 };
